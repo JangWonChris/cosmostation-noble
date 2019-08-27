b0702545a62329fedade0751669bf610cf05ad07
JayB-K <jaybkim2@gmail.com>
2019-08-27 15:19:42 +0900

    Breaking Account API into 5 different APIs; need to work on GetCommission() and GetUnbondingDelegations()

--- api/mintscan/api/controllers/account.go
+++ api/mintscan/api/controllers/account.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -11,16 +10,30 @@ import (
 	"github.com/go-pg/pg"
 	"github.com/gorilla/mux"
 	"github.com/tendermint/tendermint/rpc/client"
-	"github.com/tomasen/realip"
 )
 
 // Passes requests to its respective service
 func AccountController(codec *codec.Codec, config *config.Config, db *pg.DB, router *mux.Router, rpcClient *client.HTTP) {
-	router.HandleFunc("/account/{address}", func(w http.ResponseWriter, r *http.Request) {
-		// TEST
-		clientIP := realip.FromRequest(r) // FromRequest return client's real public IP address from http request headers.
-		log.Println("GET /account/{address}: ", clientIP)
+	// router.HandleFunc("/account/{address}", func(w http.ResponseWriter, r *http.Request) {
+	// 	// TEST
+	// 	clientIP := realip.FromRequest(r) // FromRequest return client's real public IP address from http request headers.
+	// 	log.Println("GET /account/{address}: ", clientIP)
 
-		services.GetAccountInfo(codec, config, db, rpcClient, w, r)
+	// 	services.GetAccountInfo(codec, config, db, rpcClient, w, r)
+	// }).Methods("GET")
+	router.HandleFunc("/account/balance/{address}", func(w http.ResponseWriter, r *http.Request) {
+		services.GetBalance(codec, config, db, rpcClient, w, r)
+	}).Methods("GET")
+	router.HandleFunc("/account/delegations/rewards/{address}", func(w http.ResponseWriter, r *http.Request) {
+		services.GetDelegationsRewards(codec, config, db, rpcClient, w, r)
+	}).Methods("GET")
+	router.HandleFunc("/account/delegations/{address}", func(w http.ResponseWriter, r *http.Request) {
+		services.GetDelegations(codec, config, db, rpcClient, w, r)
+	}).Methods("GET")
+	router.HandleFunc("/account/commission/{address}", func(w http.ResponseWriter, r *http.Request) {
+		services.GetCommission(codec, config, db, rpcClient, w, r)
+	}).Methods("GET")
+	router.HandleFunc("/account/unbonding-delegations/{address}", func(w http.ResponseWriter, r *http.Request) {
+		services.GetUnbondingDelegations(codec, config, db, rpcClient, w, r)
 	}).Methods("GET")
 }
