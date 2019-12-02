2246ff84f9e9df68ed24b0ce7c311aa7971dd642
kogisin <jaybkim2@gmail.com>
2019-12-02 16:04:11 +0900

    Increase varchar length to 5000 to prevent length error when saving proposals

--- chain-exporter/databases/postgresql.go
+++ chain-exporter/databases/postgresql.go
@@ -26,7 +26,7 @@ func CreateSchema(db *pg.DB) error {
 		(*dtypes.TransactionInfo)(nil), (*dtypes.VoteInfo)(nil), (*dtypes.DepositInfo)(nil)} {
 		err := db.CreateTable(model, &orm.CreateTableOptions{
 			IfNotExists: true,
-			Varchar:     999, // replaces PostgreSQL data type `text` to `varchar(n)`
+			Varchar:     5000, // replaces PostgreSQL data type `text` to `varchar(n)`
 		})
 		if err != nil {
 			return err
