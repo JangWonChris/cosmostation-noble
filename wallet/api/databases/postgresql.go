a206fbe4346ca58293a03e57ba623b6fdd743678
kogisin <jaybkim2@gmail.com>
2019-12-18 18:56:40 +0900

    Add database indexes for account table

--- wallet/api/databases/postgresql.go
+++ wallet/api/databases/postgresql.go
@@ -20,7 +20,7 @@ func ConnectDatabase(Config *config.Config) *pg.DB {
 }
 
 // Create tables if it doesn't already exist
-func CreateSchema(DB *pg.DB) error {
+func CreateSchema(db *pg.DB) error {
 	for _, model := range []interface{}{(*models.Account)(nil), (*models.AppVersion)(nil)} {
 		// disable pluralization
 		orm.SetTableNameInflector(func(s string) string {
@@ -28,13 +28,34 @@ func CreateSchema(DB *pg.DB) error {
 		})
 
 		// create tables
-		err := DB.CreateTable(model, &orm.CreateTableOptions{
+		err := db.CreateTable(model, &orm.CreateTableOptions{
 			IfNotExists: true,
 			Varchar:     999, // replaces PostgreSQL data type `text` with `varchar(n)`
 		})
+
 		if err != nil {
 			panic(err)
 		}
 	}
+
+	// runs a function in a transaction.
+	// if function returns an error transaction is rollbacked, otherwise transaction is committed.
+	// create indexes to reduce the cost of lookup queries in case of server traffic jams (B-Tree Index)
+	err := db.RunInTransaction(func(tx *pg.Tx) error {
+		_, err := db.Model(models.Account{}).Exec(`CREATE INDEX account_address_idx ON account USING btree(address);`)
+		if err != nil {
+			return err
+		}
+		_, err = db.Model(models.Account{}).Exec(`CREATE INDEX account_alarm_token_idx ON account USING btree(alarm_token);`)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
