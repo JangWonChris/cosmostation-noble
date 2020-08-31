2a2046c33b4825bc4b81e9e3f2e9ac6402d63fd3
kogisin <jaybkim2@gmail.com>
2020-08-31 15:42:33 +0900

    [mintscan] clean up client and use general codec instead of auth module codec.

--- mintscan/codec/codec.go
+++ mintscan/codec/codec.go
@@ -1,17 +1,15 @@
 package codec
 
 import (
-	_ "github.com/cosmos/cosmos-sdk/codec"
-	_ "github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/simapp"
 )
 
-// Codec is the application-wide Amino codec for serializing interfaces and data
-// var Codec *codec.Codec
+// Codec is the application-wide Amino codec for serializing interfaces and data.
+var Codec *codec.Codec
 
-/*
-// initializes upon package loading
+// initializes upon package loading.
 func init() {
 	Codec = simapp.MakeCodec()
 	Codec.Seal()
 }
-*/
