b379730a76b462a3ce2a33bd42129ccf0160e75e
carameleon <parkjung6209@gmail.com>
2020-12-03 17:38:46 +0900

    update chain-exporter

--- chain-exporter/client/staking.go
+++ chain-exporter/client/staking.go
@@ -3,10 +3,15 @@ package client
 import (
 	"context"
 	"fmt"
-	"log"
 	"sort"
 
+	// cosmos-sdk
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+
+	// internal
+	"github.com/cosmostation/cosmostation-cosmos/chain-exporter/codec"
 	"github.com/cosmostation/cosmostation-cosmos/chain-exporter/schema"
 	"github.com/cosmostation/cosmostation-cosmos/chain-exporter/types"
 )
@@ -59,22 +64,27 @@ func (c *Client) GetValidatorsByStatus(status stakingtypes.BondStatus) (validato
 		if err != nil {
 			return []schema.Validator{}, fmt.Errorf("failed to convert address from validator Address : %s", err)
 		}
-		log.Println("conspubkey get cached value : ", val.ConsensusPubkey.GetCachedValue())
-		conspubkey, err := val.TmConsPubKey()
+
+		var conspubkey cryptotypes.PubKey
+		codec.AppCodec.UnpackAny(val.ConsensusPubkey, &conspubkey)
+
+		valconspub, err := sdktypes.Bech32ifyPubKey(sdktypes.Bech32PubKeyTypeConsPub, conspubkey)
 		if err != nil {
 			return []schema.Validator{}, fmt.Errorf("failed to get consesnsus pubkey : %s", err)
 		}
-		consAddr, err := types.ConvertConsAddrFromConsPubkey(conspubkey.Address().String())
-		if err != nil {
-			return []schema.Validator{}, fmt.Errorf("failed to convert cons address from conspubkey : %s", err)
-		}
+
+		// log.Println("conspubkey get cached value : ", val.ConsensusPubkey.GetCachedValue())
+		// conspubkey, err := val.TmConsPubKey()
+		// if err != nil {
+		// 	return []schema.Validator{}, fmt.Errorf("failed to get consesnsus pubkey : %s", err)
+		// }
 
 		v := schema.NewValidator(schema.Validator{
 			Rank:                 i + 1,
 			OperatorAddress:      val.OperatorAddress,
 			Address:              accAddr,
-			ConsensusPubkey:      conspubkey.Address().String(),
-			Proposer:             consAddr,
+			ConsensusPubkey:      valconspub,
+			Proposer:             conspubkey.Address().String(),
 			Jailed:               val.Jailed,
 			Status:               int(val.Status),
 			Tokens:               val.Tokens.String(),
@@ -94,229 +104,9 @@ func (c *Client) GetValidatorsByStatus(status stakingtypes.BondStatus) (validato
 
 		validators = append(validators, *v)
 	}
-	// resp, err := c.apiClient.R().Get("/staking/validators?status=bonded")
-	// if err != nil {
-	// 	return []schema.Validator{}, fmt.Errorf("failed to request bonded vals: %s", err)
-	// }
-
-	// var bondedVals []types.Validator
-	// err = json.Unmarshal(types.ReadRespWithHeight(resp).Result, &bondedVals)
-	// if err != nil {
-	// 	return []schema.Validator{}, fmt.Errorf("failed to unmarshal bonded vals: %s", err)
-	// }
-
-	// Sort bondedVals by highest token amount
-	// sort.Slice(bondedVals[:], func(i, j int) bool {
-	// 	tempTk1, _ := strconv.Atoi(bondedVals[i].Tokens)
-	// 	tempTk2, _ := strconv.Atoi(bondedVals[j].Tokens)
-	// 	return tempTk1 > tempTk2
-	// })
-
-	// if len(bondedVals) <= 0 {
-	// 	return []schema.Validator{}, nil
-	// }
-
-	// for i, val := range bondedVals {
-	// 	accAddr, _ := types.ConvertAccAddrFromValAddr(val.OperatorAddress)
-	// 	consAddr, _ := types.ConvertConsAddrFromConsPubkey(val.ConsensusPubkey)
-
-	// 	v := schema.NewValidator(schema.Validator{
-	// 		Rank:                 i + 1,
-	// 		OperatorAddress:      val.OperatorAddress,
-	// 		Address:              accAddr,
-	// 		ConsensusPubkey:      val.ConsensusPubkey,
-	// 		Proposer:             consAddr,
-	// 		Jailed:               val.Jailed,
-	// 		Status:               val.Status,
-	// 		Tokens:               val.Tokens,
-	// 		DelegatorShares:      val.DelegatorShares,
-	// 		Moniker:              val.Description.Moniker,
-	// 		Identity:             val.Description.Identity,
-	// 		Website:              val.Description.Website,
-	// 		Details:              val.Description.Details,
-	// 		UnbondingHeight:      val.UnbondingHeight,
-	// 		UnbondingTime:        val.UnbondingTime,
-	// 		CommissionRate:       val.Commission.CommissionRates.Rate,
-	// 		CommissionMaxRate:    val.Commission.CommissionRates.MaxRate,
-	// 		CommissionChangeRate: val.Commission.CommissionRates.MaxChangeRate,
-	// 		MinSelfDelegation:    val.MinSelfDelegation,
-	// 		UpdateTime:           val.Commission.UpdateTime,
-	// 	})
-
-	// 	validators = append(validators, *v)
-	// }
 
 	return validators, nil
 }
-
-// GetBondedValidators returns all bonded validators
-// func (c *Client) GetBondedValidators() (validators []schema.Validator, err error) {
-// 	resp, err := c.apiClient.R().Get("/staking/validators?status=bonded")
-// 	if err != nil {
-// 		return []schema.Validator{}, fmt.Errorf("failed to request bonded vals: %s", err)
-// 	}
-
-// 	var bondedVals []types.Validator
-// 	err = json.Unmarshal(types.ReadRespWithHeight(resp).Result, &bondedVals)
-// 	if err != nil {
-// 		return []schema.Validator{}, fmt.Errorf("failed to unmarshal bonded vals: %s", err)
-// 	}
-
-// 	// Sort bondedVals by highest token amount
-// 	sort.Slice(bondedVals[:], func(i, j int) bool {
-// 		tempTk1, _ := strconv.Atoi(bondedVals[i].Tokens)
-// 		tempTk2, _ := strconv.Atoi(bondedVals[j].Tokens)
-// 		return tempTk1 > tempTk2
-// 	})
-
-// 	if len(bondedVals) <= 0 {
-// 		return []schema.Validator{}, nil
-// 	}
-
-// 	for i, val := range bondedVals {
-// 		accAddr, _ := types.ConvertAccAddrFromValAddr(val.OperatorAddress)
-// 		consAddr, _ := types.ConvertConsAddrFromConsPubkey(val.ConsensusPubkey)
-
-// 		v := schema.NewValidator(schema.Validator{
-// 			Rank:                 i + 1,
-// 			OperatorAddress:      val.OperatorAddress,
-// 			Address:              accAddr,
-// 			ConsensusPubkey:      val.ConsensusPubkey,
-// 			Proposer:             consAddr,
-// 			Jailed:               val.Jailed,
-// 			Status:               val.Status,
-// 			Tokens:               val.Tokens,
-// 			DelegatorShares:      val.DelegatorShares,
-// 			Moniker:              val.Description.Moniker,
-// 			Identity:             val.Description.Identity,
-// 			Website:              val.Description.Website,
-// 			Details:              val.Description.Details,
-// 			UnbondingHeight:      val.UnbondingHeight,
-// 			UnbondingTime:        val.UnbondingTime,
-// 			CommissionRate:       val.Commission.CommissionRates.Rate,
-// 			CommissionMaxRate:    val.Commission.CommissionRates.MaxRate,
-// 			CommissionChangeRate: val.Commission.CommissionRates.MaxChangeRate,
-// 			MinSelfDelegation:    val.MinSelfDelegation,
-// 			UpdateTime:           val.Commission.UpdateTime,
-// 		})
-
-// 		validators = append(validators, *v)
-// 	}
-
-// 	return validators, nil
-// }
-
-// GetUnbondingValidators returns unbonding validators
-// func (c *Client) GetUnbondingValidators() (validators []schema.Validator, err error) {
-// 	resp, err := c.apiClient.R().Get("/staking/validators?status=unbonding")
-// 	if err != nil {
-// 		return []schema.Validator{}, fmt.Errorf("failed to request unbonding vals: %s", err)
-// 	}
-
-// 	var unbondingVals []*types.Validator
-// 	err = json.Unmarshal(types.ReadRespWithHeight(resp).Result, &unbondingVals)
-// 	if err != nil {
-// 		return []schema.Validator{}, fmt.Errorf("failed to unmarshal unbonding vals: %s", err)
-// 	}
-
-// 	// Sort bondedValidators by highest token amount
-// 	sort.Slice(unbondingVals[:], func(i, j int) bool {
-// 		tempTk1, _ := strconv.Atoi(unbondingVals[i].Tokens)
-// 		tempTk2, _ := strconv.Atoi(unbondingVals[j].Tokens)
-// 		return tempTk1 > tempTk2
-// 	})
-
-// 	if len(unbondingVals) <= 0 {
-// 		return []schema.Validator{}, nil
-// 	}
-
-// 	for _, val := range unbondingVals {
-// 		accAddr, _ := types.ConvertAccAddrFromValAddr(val.OperatorAddress)
-// 		consAddr, _ := types.ConvertConsAddrFromConsPubkey(val.ConsensusPubkey)
-
-// 		v := schema.NewValidator(schema.Validator{
-// 			OperatorAddress:      val.OperatorAddress,
-// 			Address:              accAddr,
-// 			ConsensusPubkey:      val.ConsensusPubkey,
-// 			Proposer:             consAddr,
-// 			Jailed:               val.Jailed,
-// 			Status:               val.Status,
-// 			Tokens:               val.Tokens,
-// 			DelegatorShares:      val.DelegatorShares,
-// 			Moniker:              val.Description.Moniker,
-// 			Identity:             val.Description.Identity,
-// 			Website:              val.Description.Website,
-// 			Details:              val.Description.Details,
-// 			UnbondingHeight:      val.UnbondingHeight,
-// 			UnbondingTime:        val.UnbondingTime,
-// 			CommissionRate:       val.Commission.CommissionRates.Rate,
-// 			CommissionMaxRate:    val.Commission.CommissionRates.MaxRate,
-// 			CommissionChangeRate: val.Commission.CommissionRates.MaxChangeRate,
-// 			MinSelfDelegation:    val.MinSelfDelegation,
-// 			UpdateTime:           val.Commission.UpdateTime,
-// 		})
-
-// 		validators = append(validators, *v)
-// 	}
-
-// 	return validators, nil
-// }
-
-// GetUnbondedValidators returns unbonded validators
-// func (c *Client) GetUnbondedValidators() (validators []schema.Validator, err error) {
-// 	resp, err := c.apiClient.R().Get("/staking/validators?status=unbonded")
-// 	if err != nil {
-// 		return []schema.Validator{}, fmt.Errorf("failed to request unbonded vals: %s", err)
-// 	}
-
-// 	var unbondedVals []types.Validator
-// 	err = json.Unmarshal(types.ReadRespWithHeight(resp).Result, &unbondedVals)
-// 	if err != nil {
-// 		return []schema.Validator{}, fmt.Errorf("failed to unmarshal unbonded vals: %s", err)
-// 	}
-
-// 	// Sort bondedValidators by highest token amount
-// 	sort.Slice(unbondedVals[:], func(i, j int) bool {
-// 		tempTk1, _ := strconv.Atoi(unbondedVals[i].Tokens)
-// 		tempTk2, _ := strconv.Atoi(unbondedVals[j].Tokens)
-// 		return tempTk1 > tempTk2
-// 	})
-
-// 	if len(unbondedVals) <= 0 {
-// 		return []schema.Validator{}, nil
-// 	}
-
-// 	for _, val := range unbondedVals {
-// 		accAddr, _ := types.ConvertAccAddrFromValAddr(val.OperatorAddress)
-// 		consAddr, _ := types.ConvertConsAddrFromConsPubkey(val.ConsensusPubkey)
-
-// 		v := schema.NewValidator(schema.Validator{
-// 			OperatorAddress:      val.OperatorAddress,
-// 			Address:              accAddr,
-// 			ConsensusPubkey:      val.ConsensusPubkey,
-// 			Proposer:             consAddr,
-// 			Jailed:               val.Jailed,
-// 			Status:               val.Status,
-// 			Tokens:               val.Tokens,
-// 			DelegatorShares:      val.DelegatorShares,
-// 			Moniker:              val.Description.Moniker,
-// 			Identity:             val.Description.Identity,
-// 			Website:              val.Description.Website,
-// 			Details:              val.Description.Details,
-// 			UnbondingHeight:      val.UnbondingHeight,
-// 			UnbondingTime:        val.UnbondingTime,
-// 			CommissionRate:       val.Commission.CommissionRates.Rate,
-// 			CommissionMaxRate:    val.Commission.CommissionRates.MaxRate,
-// 			CommissionChangeRate: val.Commission.CommissionRates.MaxChangeRate,
-// 			MinSelfDelegation:    val.MinSelfDelegation,
-// 			UpdateTime:           val.Commission.UpdateTime,
-// 		})
-
-// 		validators = append(validators, *v)
-// 	}
-
-// 	return validators, nil
-// }
 
 // GetDelegatorDelegations returns a list of delegations made by a certain delegator address
 func (c *Client) GetDelegatorDelegations(address string) (*stakingtypes.QueryDelegatorDelegationsResponse, error) {
--- chain-exporter/go.mod
+++ chain-exporter/go.mod
@@ -6,13 +6,16 @@ require (
 	github.com/cosmos/cosmos-sdk v0.40.0-rc3
 	github.com/cosmos/gaia v0.0.1-0.20201106203234-204fe7e42f4a
 	github.com/go-pg/pg v8.0.4+incompatible
-	github.com/go-pg/pg/v10 v10.6.2
 	github.com/go-resty/resty/v2 v2.3.0
+	github.com/google/go-cmp v0.5.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/onsi/ginkgo v1.14.2 // indirect
+	github.com/onsi/gomega v1.10.3 // indirect
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
 	github.com/tendermint/tendermint v0.34.0-rc6
 	go.uber.org/zap v1.13.0
+	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb // indirect
 	google.golang.org/grpc v1.33.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	mellium.im/sasl v0.2.1 // indirect
